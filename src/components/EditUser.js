eba6d9d62b59b05e111f8944172b50d97121d2fb
Valentin-Torres-valenzuela <torresvalenzuelavalentin@gmail.com>
2022-10-19 20:58:32 -0300

    aaaaaa

--- src/components/EditUser.js
+++ src/components/EditUser.js
@@ -3,6 +3,7 @@ import { useParams, useNavigate } from 'react-router-dom';
 import axios from './axios';
 import {Link} from 'react-router-dom';
 import Swal from 'sweetalert2';
+import { isAuth } from './Auth';
 
 
 const EditUser = () => {
