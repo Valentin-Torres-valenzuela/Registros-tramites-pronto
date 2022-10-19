baccbe059425632c3e0b5fc7fa1e62bce41bc676
Valentin-Torres-valenzuela <torresvalenzuelavalentin@gmail.com>
2022-10-19 20:54:56 -0300

    aaa

--- src/components/axios.js
+++ src/components/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const prod = process.env.REACT_APP_PROD === "1"
-const prodLink = ''
+const prodLink = 'https://tramitespronto.com.ar/api/'
 const testLink = 'http://localhost:5000/api/'
 
 const instance = axios.create({
