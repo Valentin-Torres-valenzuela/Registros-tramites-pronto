3510fbd94a27376da2edad666864b2c6ddec188a
Valentin-Torres-valenzuela <torresvalenzuelavalentin@gmail.com>
2022-10-31 17:48:50 -0300

    cambios en el front de las facturas

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import logo from '../img/tramites.png';
+import Styles from './Styles.css'
 
 const User = ({user}) => {
 
@@ -8,7 +9,7 @@ const User = ({user}) => {
 
     const listaServicios = () => {
         return (
-                <table class="table table-bordered table-striped">
+                <table class="table table-bordered table-striped m-0">
                         <thead>
                             <tr>
                             <th scope="col">Servicio</th>
@@ -60,9 +61,9 @@ const User = ({user}) => {
                         <hr/> */}
                 
             <div className="card my-3">
-                <div className='row flex-row p-3'>
+                <div className='row flex-row p-0'>
                     <div className='col-5 d-flex flex-column align-items-center'>
-                        <a className="my-2"><img src={logo} alt="logo" /></a>
+                        <a className="my-2"><img src={logo} width="150" alt="logo" /></a>
                         <i>de Cecilia Noemi Raiola</i>
                         <p className="m-0">Tramitespronto02@gmail.com</p>
                         <p className="m-0">Moine 1499 (1661) - Bella vista</p>
@@ -70,14 +71,14 @@ const User = ({user}) => {
                         <p className="m-0">RESPONSABLE MONOTRIBUTO</p>
                     </div>
                     <div className="col-2 d-flex flex-column text-center border">
-                        <h2 className="h-50">X</h2>
-                        <small className="m-0 ">Documento no valido como factura</small>
+                        <h4 className="h-25">X</h4>
+                        <small className="m-0">Documento no valido como factura</small>
                     </div>
                     <div className="col-5 d-flex flex-column text-center">
                         <p className="m-0">Comprobante de pago por Cuenta y Orden de terceros</p>
-                            <ul className="list-group">
-                                <li className="list-group-item bg-secondary text-white rounded-0 border-0"> <strong>Numero de recibo: </strong> Nº 0001 - 00{user.numRecibo}</li>
-                                <li className="list-group-item border-0"> <strong>Fecha de carga: </strong> {extraida}</li>
+                            <ul className="list-group ">
+                                <li className=" list-group-item bg-secondary text-white rounded-0 border-0 p-0"> <strong>Numero de recibo: </strong> Nº 0001 - 00{user.numRecibo}</li>
+                                <li className=" list-group-item border-0 p-0"> <strong>Fecha de carga: </strong> {extraida}</li>
                             </ul>
                         <p className="m-0">C.U.I.T: 27-22014816-0</p>
                         <p className="m-0">Ing. Brutos: 27-22014816-0</p>
@@ -86,28 +87,28 @@ const User = ({user}) => {
                 </div>
                     <div className="d-flex my-2 col-12">
                         <ul className="list-group w-100">
-                            <li className="list-group-item"> <strong>Nombre y apellido: </strong> {user.nombre}</li>
+                            <li className="list-group-item p-1 rounded-0"> <strong>Nombre y apellido: </strong> {user.nombre}</li>
                             <div className="d-flex">
-                                <li className="list-group-item w-50"> <strong>C.U.I.T: </strong></li>
-                                <li className="list-group-item w-50"> <strong>I.V.A: </strong></li>
+                                <li className="list-group-item w-50 p-1"> <strong>C.U.I.T: </strong></li>
+                                <li className="list-group-item w-50 p-1"> <strong>I.V.A: </strong></li>
                             </div>
-                            <li className="list-group-item w-100"> <strong>Recibimos la suma de $: </strong></li>
+                            <li className="list-group-item w-100 rounded-0 p-1"> <strong>Recibimos la suma de $: </strong></li>
                         </ul>
                     </div>
                     <div className="col-12">
-                        <p className="m-2">En concepto de pago de los siguientes servicios, impuestos, contribuciones, etc:</p>
+                        <p className="p-0 m-0">En concepto de pago de los siguientes servicios, impuestos, contribuciones, etc:</p>
                         <ul className="list-group w-100">
                             <li className="list-group-item p-0 border-0"> {listaServicios()} </li>
                         </ul>
                     </div>
-                    <div className="d-flex col-12 my-3">
+                    <div className="d-flex col-12 my-1">
                         <ul className="list-group w-100">
-                            <li className="list-group-item"> <strong>Total pagos a efectuar: </strong> {user.totalPagosEfectuar}</li>
-                            <li className="list-group-item"> <strong>Arancel: </strong> {user.arancel}</li>
-                            <li className="list-group-item bg-success text-white rounded-0 h5 m-0"> <strong>Total: </strong> {user.total}</li>
+                            <li className="list-group-item p-1 rounded-0"> <strong>Total pagos a efectuar: </strong> {user.totalPagosEfectuar}</li>
+                            <li className="list-group-item p-1"> <strong>Arancel: </strong> {user.arancel}</li>
+                            <li className="list-group-item bg-success text-white rounded-0 m-0 p-1"> <strong>Total: </strong> {user.total}</li>
                         </ul>
                     </div>
-                        <Link to={`/editar/${user._id}`} className="btn btn-sm btn-warning rounded-0 text-center"> <strong>Editar </strong> <i className="fa-solid fa-pen-to-square"></i></Link>
+                        <Link to={`/editar/${user._id}`} className="btn sm btn-sm btn-warning rounded-0 text-center"> <strong>Editar </strong> <i className="fa-solid fa-pen-to-square"></i></Link>
                         {/* <button className="btn btn-info rounded-0 my-2 text-center" onClick={() => window.print()}> <strong>Imprimir </strong> <i class="fa-solid fa-print"></i></button> */}
             </div>
         </>
